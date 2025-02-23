85e8b93aa0d7ad7749aeb8b893909e46a5c72a9d
liem nguyen <elminguyen@gmail.com>
2025-02-23 23:32:17 +0200

    make route for reports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const module = await import('./home/home.component');
+      return module.HomeComponent;
+    }
+  },
+  {
+    path: 'reports',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const module = await import('./reports/reports.component');
+      return module.ReportsComponent;
+    }
+  }
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TimerComponent } from "../components/timer/timer.component";
 import { TasksComponent } from "../components/tasks/tasks.component";
+import { ReportsComponent } from "../reports/reports.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TimerComponent, TasksComponent],
+  imports: [TimerComponent, TasksComponent, ReportsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
